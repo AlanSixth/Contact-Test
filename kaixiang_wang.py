82ad93a01f0474153cb0c3fb091462b9447b822a
AlanSixth <546568303@qq.com>
2016-06-09 11:48:32 +1000

    Add files via upload

---
+++ kaixiang_wang.py
@@ -0,0 +1,10 @@
+kaixiang_wang={'name':'Kaixiang Wang',
+               'area_of_research':'control engineering',
+               'institution':'University of Melbourne',
+               'coffee':'cappuccino',
+               'pet_name':'',
+               'date_of_workshop':'8/6/2016-9/6/2016',
+              'email':'kaixiangw@student.unimelb.edu.au'}
+# add an additional key
+kaixiang_wang['comment']='No comment. '*16
+
